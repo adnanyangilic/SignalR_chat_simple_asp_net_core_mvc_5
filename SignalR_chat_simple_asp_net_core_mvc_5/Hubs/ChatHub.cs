a09b55eebcd5c41ce8b5386adcfb499a80b28650
adnanyangilic <adnan.alistika@gmail.com>
2022-05-31 22:42:15 +0300

    Proje dosyası ekle.

---
+++ SignalR_chat_simple_asp_net_core_mvc_5/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_chat_simple_asp_net_core_mvc_5.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string chatuser, string chatmessage)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", chatuser, chatmessage);
+        }
+    }
+}
